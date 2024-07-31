a460b8deed9aaf03caeb8a936e19a0aa079ccfe4
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-07-30 23:34:36 -0700

    add saints song

---
+++ index.js
@@ -0,0 +1,33 @@
+// console.log("hello world");
+
+// Don't Repeat Yourself (DRY)
+function refrain() {
+    console.log("Oh when the saints,");
+    console.log("Go marching in.")
+    console.log("Oh when the saints,");
+    console.log("Go marching in.");
+    console.log("I want to be in that number.");
+    console.log("When the saints go marching in.");
+}
+
+// Refrain
+
+refrain();
+
+// Verse 1
+console.log("Oh when the sun refuse to shine");
+console.log("Oh when the sun refuse to shine");
+console.log("Oh I want to be in that number.");
+console.log("When the sun refuse to shine.");
+
+// Refrain
+refrain();
+
+// Verse 2
+console.log("When the stars fall from the sky");
+console.log("Oh When the stars fall from the sky");
+console.log("Oh I want to be in that number.");
+console.log("When the stars fall from the sky");
+
+// Refrain
+refrain();
